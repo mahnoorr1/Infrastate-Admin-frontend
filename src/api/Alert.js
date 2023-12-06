587534ba2e5fb74318c4d9c5eaa055f5aaeba41b
hamza <hs623965@gmail.com>
2023-12-06 14:55:20 +0500

    mail

--- src/api/Alert.js
+++ src/api/Alert.js
@@ -57,6 +57,33 @@ export const getOneAlert = async (aid) => {
     }
   }
 
+export const MailUser = async (aid) => {
+
+    try {
+      const response = await Axios.post(`${baseURL}/mailUser/${aid}`);
+      return response.data;
+  
+  
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        if (error.response.data.message === 'Not Authorized No Token.') {
+          const errorMessage = error.response.data.message + ' Please Login First';
+          alert(errorMessage);
+          window.location.href = '/';
+        } else if (error.response.data.message === 'UnAuthorized Token.') {
+          const errorMessage = "You don't have access to this page.";
+          alert(errorMessage);
+          window.location.href = '/Home';
+        } else {
+          alert(error.response.data.message);
+        }
+      } else {
+        alert("An error occurred. Please try again later.");
+      }
+      
+      return null; // Return null in case of an error
+    }
+  }
   
 export const deleteAlert = async (aid) => {
 
